6f9e495467061c29df20573a90f8d049f5c525d0
Poultrylab <1746711217@qq.com>
2025-02-11 11:57:23 +0800

    Create Analysis in Linux.sh

---
+++ Analysis in Linux.sh
@@ -0,0 +1,27 @@
+
+
+#1. RNA-Seq alignment and sorting using HISAT2 and Samtools.
+#hisat2-2.2.1 samtools-1.11
+
+cd ${cleanPath}
+for file in `cat file`
+do
+
+$hisat/hisat2 -p 20 -x $ref -1 ${file}_1.fq.gz -2 ${file}_2.fq.gz |$samtools/samtools sort -@ 8 -o $out/${file}.sorted.bam
+done
+
+
+# High-Throughput Transcript Quantification Pipeline Using StringTie v2.1.5: Automated Multi-Threaded Assembly and Expression Analysis
+module load StringTie/2.1.5
+
+cd ${cleanPath}
+for file in `cat file`
+do
+
+stringtie -p 20 -e -G $gtf  -o $out/${file}.gtf  -A $out/${file}.tsv $input/${file}.sorted.bam
+done
+
+
+#stringtie-2.1.4
+
+python $stringtie/prepDE.py -i file_list.txt -g $out/zong_gene_count_matrix.csv -t $out/zong_transcript_count_matrix.csv
